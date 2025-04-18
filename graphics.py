f8731cdd4dd51bd4b20bd4b37a56cbd1e025f081
Vladimir Rujević <vladimir.rujevic@live.com>
2025-04-18 21:23:19 +0200

    Add Point and Line classes, draw example lines
    - rename window.py to graphics.py

--- graphics.py
+++ graphics.py
@@ -1,5 +1,18 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+
 class Window():
     def __init__(self, width, height):
         self.__root = Tk()
@@ -10,6 +23,8 @@ class Window():
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
     def redraw(self):
         self.__root.update()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from window import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line_a = Line(Point(100, 100), Point(200, 200))
+    win.draw_line(line_a, "black")
+    line_b = Line(Point(100, 200), Point(200, 100))
+    win.draw_line(line_b, "red")
     win.wait_for_close()
 
 if __name__ == "__main__":
